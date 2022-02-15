21a55b36f3667743248ef072b5caf5ac09020ffa
Ivan Bowen <ivanovicbow@gmail.com>
2022-02-15 11:21:12 +0000

    Sessions and Cookies in Django

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = [ '*' ]
 # Application definition
 
 INSTALLED_APPS = [
+    'hello.apps.HelloConfig',
+    'home.apps.HomeConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -46,8 +46,8 @@ def vote(request, question_id):
             'error_message' : "You didn't select a choice.",
             })
     else:
-        # selected_choice.votes = F('votes') + 1
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
+        #selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
