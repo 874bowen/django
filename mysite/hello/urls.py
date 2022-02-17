014eb6949e9dcc82274fe01152fa14084cbc051f
Ivan Bowen <ivanovicbow@gmail.com>
2022-02-17 21:20:50 +0000

    sending emails in django, Yeey it worked

--- mysite/hello/urls.py
+++ mysite/hello/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-
+from django.urls import include, path
+from django.views.generic.base import TemplateView
 from . import views
 
 app_name = 'hello'
 urlpatterns = [
     path('', views.sessfun),
+    path('email/',views.email, name='email'),
     ]
--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 # Create your views here.
 def sessfun(request):
@@ -9,3 +10,7 @@ def sessfun(request):
     resp = HttpResponse('view count='+str(num_visits))
     resp.set_cookie('dj4e_cookie', '4c26b0f5', max_age=1000)
     return resp
+
+def email(request):
+    send_mail('Hey, Sending from Ivan', 'Hi Bowen, how are you doing. Yeey!! You learnt django mails', 'ivanovicbow850@gmail.com', ['bowenivan16@gmail.com'], fail_silently=False)
+    return render(request, 'hello/email.html')
