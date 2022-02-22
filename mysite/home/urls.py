a51985ecbca2cd72d0a66ef3766a33051505c76e
Ivan Bowen <ivanovicbow@gmail.com>
2022-02-22 09:15:06 +0000

    ads application with favicon and mysql client not sqlite3

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
 from . import views
+from django.contrib.auth import views as auth_views
 
-app_name = 'home'
+urlpatterns = [
+    path('', views.HomeView.as_view()),
+]
